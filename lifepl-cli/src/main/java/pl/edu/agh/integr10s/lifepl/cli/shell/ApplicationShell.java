ad2f8e3d70fe53823e653b311b2cea9b9d357b03
Yaroslav Machkivskiy <ihoo.ir@gmail.com>
2015-05-03 23:42:31 +0200

    CLI - refactorization of process of building shells

--- lifepl-cli/src/main/java/pl/edu/agh/integr10s/lifepl/cli/shell/ApplicationShell.java
+++ lifepl-cli/src/main/java/pl/edu/agh/integr10s/lifepl/cli/shell/ApplicationShell.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.*;
 
 public final class ApplicationShell {
+
     private static final Logger logger = LoggerFactory.getLogger(ApplicationShell.class);
 
     private static final String APP_NAME = "Lifepl - application for integration of plans on fixed world model";
@@ -29,8 +30,74 @@ public final class ApplicationShell {
     }
 
     private static class AppShellBuilder {
+
         private final Set<SubShell> subShells = new HashSet<>();
         private final String applicationName;
+
+        private static final class BuildEngine {
+
+            List<SubShell> mainCategoryShells = new LinkedList<>();
+            Map<SubShellName, List<SubShell>> parentSubShellNameToSubShellsMap = new HashMap<>();
+
+            public BuildEngine() {
+                for (SubShellName subShellName : SubShellName.values()) {
+                    parentSubShellNameToSubShellsMap.put(subShellName, new LinkedList<>());
+                }
+            }
+
+            public void processSubShells(Set<SubShell> subShells) {
+                for (SubShell subShell : subShells) {
+                    final SubShellName subShellName = subShell.getSubShellName();
+                    final SubShellName parentSubShellName = subShell.getParentSubShellName();
+
+                    logger.debug("has declaration of sub shell  ' {} '  with name  ' {} '  and parent sub shell  ' {} ' ", subShell, subShellName, parentSubShellName);
+
+                    parentSubShellNameToSubShellsMap.get(parentSubShellName).add(subShell);
+
+                    if (subShellName == SubShellName.MAIN) {
+                        logger.debug("found root sub shell  ' {} '  ", subShell);
+                        mainCategoryShells.add(subShell);
+                    }
+                }
+            }
+
+            public void injectSubShellsDependencies(SubShell rootLevelElement) {
+
+                Stack<SubShell> unconfiguredSubShells = new Stack<>();
+
+                unconfiguredSubShells.push(rootLevelElement);
+
+                while (!unconfiguredSubShells.isEmpty()) {
+                    SubShell currentSubShell = unconfiguredSubShells.pop();
+                    logger.debug("configuring dependencies for sub shell  ' {} ' ", currentSubShell);
+                    List<SubShell> childSubShells = parentSubShellNameToSubShellsMap.get(currentSubShell.getSubShellName());
+                    if (childSubShells.isEmpty()) {
+                        logger.debug("sub shell  ' {} '  has no child sub shells", currentSubShell);
+                    }
+
+                    for (SubShell childSubShell : childSubShells) {
+                        logger.debug("found child sub shell  ' {} ' ", childSubShell);
+                        currentSubShell.addChildSubShell(childSubShell);
+                        unconfiguredSubShells.push(childSubShell);
+                    }
+                }
+            }
+
+
+            public SubShell peekRootSubShell() {
+                if (mainCategoryShells.isEmpty()) {
+                    logger.error("no root level sub shell found during building application shell");
+                    System.exit(1);
+                } else if (mainCategoryShells.size() > 1) {
+                    logger.warn("found more than one sub shell of root level, will peek first found as root");
+                }
+                return mainCategoryShells.get(0);
+            }
+
+            public List<SubShell> getMainCategoryShells() {
+                return mainCategoryShells;
+            }
+        }
 
         public AppShellBuilder(String applicationName) {
             this.applicationName = applicationName;
@@ -43,71 +110,23 @@ public final class ApplicationShell {
         }
 
         public ApplicationShell build() {
+            BuildEngine buildEngine = new BuildEngine();
 
             logger.debug("start building application shell");
 
-            EnumSet<SubShellName> declaredNames = EnumSet.noneOf(SubShellName.class);
-            List<SubShell> mainCategoryShells = new LinkedList<>();
-            Map<SubShellName, List<SubShell>> parentSubShellNameToSubShellsMap = new HashMap<>();
-
-            for (SubShellName subShellName : SubShellName.values()) {
-                parentSubShellNameToSubShellsMap.put(subShellName, new LinkedList<>());
-            }
-
-            for (SubShell subShell : subShells) {
-                final SubShellName subShellName = subShell.getSubShellName();
-                final SubShellName parentSubShellName = subShell.getParentSubShellName();
-
-                logger.debug("found sub shell {} with name {} and parent sub shell {}", subShell, subShellName, parentSubShellName);
-
-                declaredNames.add(subShellName);
-
-                final List<SubShell> subShellNameImplementations = parentSubShellNameToSubShellsMap.get(parentSubShellName);
-                if (subShellNameImplementations.size() > 0) {
-                    logger.warn("found more than one sub shell implementation {} for name {}", subShell, subShellName);
-                }
-
-                subShellNameImplementations.add(subShell);
-
-                if (subShellName == SubShellName.MAIN) {
-                    logger.debug("found root sub shell {} ", subShell);
-                    mainCategoryShells.add(subShell);
-                }
-            }
+            buildEngine.processSubShells(subShells);
 
             logger.debug("checking dependencies of declared sub shells");
 
-            if (mainCategoryShells.isEmpty()) {
-                logger.error("no root level sub shell found during building application shell");
-                System.exit(1);
-            } else if (mainCategoryShells.size() > 1) {
-                logger.warn("found more than one sub shell of root level, will peek first found as root");
-            }
+            SubShell rootSubShell = buildEngine.peekRootSubShell();
 
-            SubShell rootLevelElement = mainCategoryShells.get(0);
+            logger.debug("use  ' {} '  sub shell as application shell root", rootSubShell);
 
-            logger.debug("use {} sub shell as application shell root", rootLevelElement);
+            buildEngine.injectSubShellsDependencies(rootSubShell);
 
-            Stack<SubShell> unconfiguredSubShells = new Stack<>();
+            logger.debug("finish building application shell");
 
-            unconfiguredSubShells.push(rootLevelElement);
-
-            while (!unconfiguredSubShells.isEmpty()) {
-                SubShell currentSubShell = unconfiguredSubShells.pop();
-                logger.debug("configuring dependencies for sub shell {}", currentSubShell);
-                List<SubShell> childSubShells = parentSubShellNameToSubShellsMap.get(currentSubShell.getSubShellName());
-                if (childSubShells.isEmpty()) {
-                    logger.debug("sub shell {} has no child sub shells", currentSubShell);
-                }
-
-                for (SubShell childSubShell : childSubShells) {
-                    logger.debug("found child sub shell {}", childSubShell);
-                    currentSubShell.addChildSubShell(childSubShell);
-                    unconfiguredSubShells.push(childSubShell);
-                }
-            }
-
-            return new ApplicationShell(rootLevelElement);
+            return new ApplicationShell(rootSubShell);
         }
     }
 
