5aa6230af11c27f5b2586b02becce1b837a226ee
Yaroslav Machkivskiy <ihoo.ir@gmail.com>
2015-05-09 19:15:10 +0200

    Infrastructure update - engines and interfaces

---
+++ lifepl-engine-common/src/main/java/pl/edu/agh/integr10s/engine/App.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.integr10s.engine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
