b2b5fc2d20732d270e0f3761bab6ec89b006f312
Yaroslav Machkivskiy <ihoo.ir@gmail.com>
2015-05-10 17:15:30 +0200

    Add worlds service to app context [CLI]

--- lifepl-cli/src/main/java/pl/edu/agh/integr10s/lifepl/cli/shell/ApplicationContext.java
+++ lifepl-cli/src/main/java/pl/edu/agh/integr10s/lifepl/cli/shell/ApplicationContext.java
@@ -2,10 +2,21 @@ package pl.edu.agh.integr10s.lifepl.cli.shell;
 
 import pl.edu.agh.integr10s.clibuilder.shell.AppContext;
 import pl.edu.agh.integr10s.lifepl.persistance.actors.ActorsService;
+import pl.edu.agh.integr10s.lifepl.persistance.worlds.WorldsService;
 
 public class ApplicationContext implements AppContext {
 
     private ActorsService actorsService;
+
+    private WorldsService worldsService;
+
+    public WorldsService getWorldsService() {
+        return worldsService;
+    }
+
+    public void setWorldsService(WorldsService worldsService) {
+        this.worldsService = worldsService;
+    }
 
     public ActorsService getActorsService() {
         return actorsService;
